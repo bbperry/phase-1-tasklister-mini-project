889303f1ad2ec44d6cf32dc3a681d79432c165ff
Ben <benbperry1@gmail.com>
2021-11-08 20:38:34 -0800

    Created todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#createTaskForm');
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDo();
+    form.reset();
+  });
 });
+
+const makeEl = el => document.createElement(el);
+
+
+function buildToDo() {
+  //let p = document.createElement('tr');
+  let btn = document.createElement("button");
+  let priority = document.querySelector("#priority").value;
+  let newListRow = makeEl("tr")
+  let newTaskData = makeEl('td')
+  let newPriorityData = makeEl('td')
+  let btnContainer = makeEl('td')
+  let currentEntry = document.querySelector('#newTaskDescription').value
+  let taskTable = document.querySelector("#tasks")
+
+    if (currentEntry == "") {
+    alert("Please Enter A Task Description!");
+    return false;
+  }
+
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "Remove List Item";
+  // newListRow.textContent = `${todo}`;
+  newTaskData.textContent = currentEntry
+  newPriorityData.textContent = `${priority}`
+  // newListRow.appendChild(btn);
+
+  document.querySelector("#list").appendChild(newListRow, priority);
+
+  newListRow.append(newTaskData, newPriorityData, btn)
+  taskTable.appendChild(newListRow)
+  // p.appendChild(priority)
+
+  console.log(currentEntry);
+
+  // document.querySelector('#priority').appendChild(priority)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
