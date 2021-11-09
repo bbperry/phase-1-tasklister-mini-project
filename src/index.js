08288fd0acc448e00be22986cda1c8a80181e201
Ben <benbperry1@gmail.com>
2021-11-08 21:27:59 -0800

    Added colors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let form = document.querySelector('#createTaskForm');
+  let form = document.querySelector("#createTaskForm");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     buildToDo();
@@ -7,36 +7,39 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-const makeEl = el => document.createElement(el);
-
+const makeEl = (el) => document.createElement(el);
 
 function buildToDo() {
-  //let p = document.createElement('tr');
-  let btn = document.createElement("button");
+    
+  let btn = makeEl("button"); 
+  let newListRow = makeEl("tr");
+  let newTaskData = makeEl("td");
+  let newPriorityData = makeEl("td");
+  let btnContainer = makeEl("td");
   let priority = document.querySelector("#priority").value;
-  let newListRow = makeEl("tr")
-  let newTaskData = makeEl('td')
-  let newPriorityData = makeEl('td')
-  let btnContainer = makeEl('td')
-  let currentEntry = document.querySelector('#newTaskDescription').value
-  let taskTable = document.querySelector("#tasks")
+  let currentEntry = document.querySelector("#newTaskDescription").value;
+  let taskTable = document.querySelector("#tasks");
 
-    if (currentEntry == "") {
+  if (currentEntry == "") {
     alert("Please Enter A Task Description!");
     return false;
   }
 
+ if (newPriorityData == 'Low Priority') {
+    newPriorityData.style.color = 'red'
+ }
+
   btn.addEventListener("click", handleDelete);
   btn.textContent = "Remove List Item";
-  // newListRow.textContent = `${todo}`;
-  newTaskData.textContent = currentEntry
-  newPriorityData.textContent = `${priority}`
-  // newListRow.appendChild(btn);
+  newTaskData.textContent = currentEntry;
+  newPriorityData.textContent = `${priority}`;
+  newListRow.className = `${priority}`;
 
-  document.querySelector("#list").appendChild(newListRow, priority);
+console.log(`p-${newPriorityData}`)
+  // document.querySelector("#list").appendChild(newListRow, priority);
 
-  newListRow.append(newTaskData, newPriorityData, btn)
-  taskTable.appendChild(newListRow)
+  newListRow.append(newTaskData, newPriorityData, btn);
+  taskTable.appendChild(newListRow);
   // p.appendChild(priority)
 
   console.log(currentEntry);
